ed299450ac8e063dc1f04c6b7b8f8733195a81d9
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2017-04-12 14:06:48 +0200

    cors

--- config/express.js
+++ config/express.js
@@ -27,6 +27,12 @@ module.exports = function(app, config) {
   app.use(express.static(config.root + '/public'));
   app.use(methodOverride());
 
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
   var controllers = glob.sync(config.root + '/app/controllers/*.js');
   controllers.forEach(function (controller) {
     require(controller)(app);
